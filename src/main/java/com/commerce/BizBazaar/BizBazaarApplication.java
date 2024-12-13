7b4807cf6a67118c5b8a5994d283ce0c0c09d42e
pjt-tech <dktkd7044@gmail.com>
2024-12-13 23:03:19 +0900

    first commit

---
+++ src/main/java/com/commerce/BizBazaar/BizBazaarApplication.java
@@ -0,0 +1,13 @@
+package com.commerce.BizBazaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizBazaarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizBazaarApplication.class, args);
+	}
+
+}
