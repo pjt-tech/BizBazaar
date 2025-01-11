c8a51e3dac973f6f694ac53eb2e981fec603834e
pjt-tech <dktkd7044@gmail.com>
2025-01-11 21:38:03 +0900

    healthCheck

---
+++ src/main/java/com/commerce/BizBazaar/healthCheck/controller/HealthCheckController.java
@@ -0,0 +1,26 @@
+package com.commerce.BizBazaar.healthCheck.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HealthCheckController {
+
+    private String env;
+
+    @GetMapping("/hc")
+    public ResponseEntity<?> healthCheck() {
+        Map<String, String> responseData = new HashMap<>();
+
+        return ResponseEntity.ok(responseData);
+    }
+
+    @GetMapping("/env")
+    public ResponseEntity<?> getEnv() {
+        return ResponseEntity.ok(env);
+    }
+}
